09c15eb9ba97cc74ce5410956410905732ba7b55
NiemiecAGH <37615892+NiemiecAGH@users.noreply.github.com>
2021-05-07 21:37:26 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please write the matching character set as a single word: ");
         String characterInput = scanner.nextLine();
-        System.out.println("Please write the fraze in which you want to find the characters: ");
+        System.out.println("Please write the phrase in which you want to find the characters: ");
         String input = scanner.nextLine();
         Identifier identifier = new Identifier(characterInput.toLowerCase(), input.toLowerCase());
         System.out.println(identifier.getOutput());
